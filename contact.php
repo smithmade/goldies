53784807edf1f4d8d2aabaf105a852bf61d8077f
Nicholas Villapiano <nick@smithmade.org>
2014-04-07 16:39:04 -0400

    analytics

--- contact.php
+++ contact.php
@@ -140,9 +140,22 @@ MADE BY
 		<a href="http://www.smithmade.org"><img src="images/coin-white.svg" alt="SMITH"></a>
 	</div>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49782095-1', 'goodgoldies.com');
+  ga('send', 'pageview');
+
+</script>
 
 
 <!-- End Document
 ================================================== -->
 </body>
+
+
+
 </html>
